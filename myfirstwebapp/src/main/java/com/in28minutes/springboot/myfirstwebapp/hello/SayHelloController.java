d803dfd16c5327c2d2ebf7e48426005b8a4f672a
Blairi <axel.avi.fer@gmail.com>
2023-03-01 22:24:07 -0600

    hello controller

---
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,30 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello, what are you learning today?";
+	}
+	
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("<h1>First html page with spring</h1>");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		return sb.toString();
+	}
+}
