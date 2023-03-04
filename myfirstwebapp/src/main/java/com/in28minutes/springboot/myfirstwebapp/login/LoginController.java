44df06e9e10fa0381303f3e6376f5d02bb18ab08
Blairi <axel.avi.fer@gmail.com>
2023-03-04 13:49:06 -0600

    simple authentication

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,39 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("login")
+	private AuthenticationService authenticationService;
+	
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
+	@RequestMapping(value="login", method = RequestMethod.GET)
 	public String goToLoginPage() {
+		return "login";
+	}
+	
+	@RequestMapping(value="login", method = RequestMethod.POST)
+	public String goToWelcomePage(@RequestParam String name, 
+			@RequestParam String password, ModelMap model) {
+		
+		if(authenticationService.authenticate(name, password)) {
+			model.put("name", name);
+			
+			// Authentication
+			
+			return "welcome";	
+		}
+		
+		model.put("invalidCredentials", "Invalid credentials. Try again.");
 		return "login";
 	}
 }
