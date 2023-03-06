a1656e2704bf57e446bd3c2e983875cd764cf61e
Blairi <axel.avi.fer@gmail.com>
2023-03-06 16:13:47 -0600

    welcome controller

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/WelcomeController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/WelcomeController.java
@@ -9,33 +9,11 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name")
-public class LoginController {
+public class WelcomeController {
 	
-	private AuthenticationService authenticationService;
-	
-	public LoginController(AuthenticationService authenticationService) {
-		super();
-		this.authenticationService = authenticationService;
-	}
-
-	@RequestMapping(value="login", method = RequestMethod.GET)
-	public String goToLoginPage() {
-		return "login";
-	}
-	
-	@RequestMapping(value="login", method = RequestMethod.POST)
-	public String goToWelcomePage(@RequestParam String name, 
-			@RequestParam String password, ModelMap model) {
-		
-		if(authenticationService.authenticate(name, password)) {
-			model.put("name", name);
-			
-			// Authentication
-			
-			return "welcome";	
-		}
-		
-		model.put("invalidCredentials", "Invalid credentials. Try again.");
-		return "login";
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String goToWelcomePage(ModelMap model) {
+		model.put("name", "Axel");
+		return "welcome";
 	}
 }
