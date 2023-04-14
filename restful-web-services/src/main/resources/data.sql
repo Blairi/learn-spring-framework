8804ec2b2020dad1a51d4a876bbd5b9ea7c3812d
Blairi <axel.avi.fer@gmail.com>
2023-04-14 14:38:54 -0600

    sql startup script

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_details (id, birth_date, name) VALUES (10001, CURRENT_DATE(), 'Ranga');
+
+INSERT INTO user_details (id, birth_date, name) VALUES (10002, CURRENT_DATE(), 'Steve');
+
+INSERT INTO user_details (id, birth_date, name) VALUES (10003, CURRENT_DATE(), 'Ravi');
