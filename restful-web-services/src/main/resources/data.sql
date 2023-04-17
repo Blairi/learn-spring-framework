54a6b772fdb558cad104208db42749a370937bfc
Blairi <axel.avi.fer@gmail.com>
2023-04-16 22:30:39 -0600

    sql script startup to the posts

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -3,3 +3,11 @@ INSERT INTO user_details (id, birth_date, name) VALUES (10001, CURRENT_DATE(), '
 INSERT INTO user_details (id, birth_date, name) VALUES (10002, CURRENT_DATE(), 'Steve');
 
 INSERT INTO user_details (id, birth_date, name) VALUES (10003, CURRENT_DATE(), 'Ravi');
+
+INSERT INTO post (id, description, user_id) VALUES (10007, 'I want learn AWS', 10001);
+
+INSERT INTO post (id, description, user_id) VALUES (10009, 'I want learn Azure', 10001);
+
+INSERT INTO post (id, description, user_id) VALUES (10000, 'I want learn Spring', 10002);
+
+INSERT INTO post (id, description, user_id) VALUES (10002, 'I want learn FullStack', 10003);
