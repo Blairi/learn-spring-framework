9c6dcba2655169168a81380b571225f71b5f52b8
Blairi <axel.avi.fer@gmail.com>
2023-05-06 22:17:33 -0600

    login component added

--- full-stack/todo-app/src/todo/TodoApp.jsx
+++ full-stack/todo-app/src/todo/TodoApp.jsx
@@ -1,6 +1,38 @@
 
 export const TodoApp = () => {
   return (
-    <div>TodoApp</div>
+    <div className="TodoApp">
+      TodoApp
+      <LoginComponent />
+      {/* <WelcomeComponent /> */}
+    </div>
+  )
+}
+
+const LoginComponent = () => {
+  return(
+    <div className="Login">
+      <div className="LoginForm">
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <div>
+          <button type="submit" name="login">login</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const WelcomeComponent = () => {
+  return(
+    <div className="Welcome">
+      Welcome component
+    </div>
   )
 }
