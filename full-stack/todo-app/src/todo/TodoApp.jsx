c9aeb61c2032ac1878a4145d8a63d3ceb3747780
Blairi <axel.avi.fer@gmail.com>
2023-05-06 22:31:02 -0600

    implemented basic functionality

--- full-stack/todo-app/src/todo/TodoApp.jsx
+++ full-stack/todo-app/src/todo/TodoApp.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import './TodoApp.css';
 
 export const TodoApp = () => {
   return (
@@ -10,16 +12,38 @@ export const TodoApp = () => {
 }
 
 const LoginComponent = () => {
+
+  const [username, setUsername] = useState('blairi');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
   return(
     <div className="Login">
       <div className="LoginForm">
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
+          <input 
+            type="text" 
+            name="username" id="username"
+            value={ username }
+            onChange={ handleUsernameChange }
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input 
+            type="password" 
+            name="password" id="password" 
+            value={ password }
+            onChange={ handlePasswordChange }
+          />
         </div>
         <div>
           <button type="submit" name="login">login</button>
