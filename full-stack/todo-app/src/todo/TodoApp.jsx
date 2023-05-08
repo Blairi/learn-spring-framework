4b33f08055b4de586056dc5754752f1eec5d3e14
Blairi <axel.avi.fer@gmail.com>
2023-05-07 22:52:09 -0600

    hardcoded authentication added

--- full-stack/todo-app/src/todo/TodoApp.jsx
+++ full-stack/todo-app/src/todo/TodoApp.jsx
@@ -15,6 +15,8 @@ const LoginComponent = () => {
 
   const [username, setUsername] = useState('blairi');
   const [password, setPassword] = useState('');
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -24,8 +26,36 @@ const LoginComponent = () => {
     setPassword(e.target.value)
   }
 
+  const handleSubmit = () => {
+    if (username === 'blairi' && password === '') {
+      console.log("success");
+      setShowSuccessMessage(true);
+      setShowErrorMessage(false);
+    }
+    else{
+      setShowSuccessMessage(false);
+      setShowErrorMessage(true);
+    }
+  }
+
+  const SuccessMessageComponent = () => {
+    if(showSuccessMessage)
+      return <div className='successMessage'>Authenticate Successfully</div>;
+    else
+      return null
+  }
+
+  const ErrorMessageComponent = () => {
+    if(showErrorMessage)
+      return <div className='errorMessage'>Authenticate Failed. Please check your credentials.</div>;
+    else
+      return null
+  }
+
   return(
     <div className="Login">
+      <SuccessMessageComponent />
+      <ErrorMessageComponent />
       <div className="LoginForm">
         <div>
           <label htmlFor="username">Username</label>
@@ -46,7 +76,10 @@ const LoginComponent = () => {
           />
         </div>
         <div>
-          <button type="submit" name="login">login</button>
+          <button 
+            type="submit" name="login" 
+            onClick={ handleSubmit }
+          >login</button>
         </div>
       </div>
     </div>
