faf10bbd6f10a8e3c1eacfb4c1c2f2baa60da365
Blairi <axel.avi.fer@gmail.com>
2023-05-15 22:06:51 -0600

    adding custom hook to handle the auth context

--- full-stack/todo-app/src/todo/security/AuthContext.js
+++ full-stack/todo-app/src/todo/security/AuthContext.js
@@ -1,13 +1,15 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
+export const useAuth = () => useContext(AuthContext)
+
 export const AuthProvider = ({ children }) => {
 
-  const [number, setNumber] = useState(0);
+  const [isAuthenticated, setAuthenticated] = useState(false);
 
   return (
-    <AuthContext.Provider value={ {number} }>
+    <AuthContext.Provider value={{ isAuthenticated, setAuthenticated }}>
       { children }
     </AuthContext.Provider>
   );
