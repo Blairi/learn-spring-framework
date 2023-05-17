267e4bca090ff30f40ea5a7ad48c5ad91ecd2b36
Blairi <axel.avi.fer@gmail.com>
2023-05-16 22:48:45 -0600

    calling api from frontend

--- full-stack/todo-app/src/todo/WelcomeComponent.jsx
+++ full-stack/todo-app/src/todo/WelcomeComponent.jsx
@@ -1,14 +1,42 @@
 import { Link, useParams } from "react-router-dom";
+import axios from 'axios';
 
 export const WelcomeComponent = () => {
 
   const { username } = useParams();
+
+  const callHelloWorldApi = () => {
+    axios.get("http://localhost:8080/hello-world")
+      .then(
+        (response) => successfulResponse (response)
+      )
+      .catch(
+        (response) => errorResponse (response)
+      )
+      .finally(
+        (response) => console.log('cleanup')
+      )
+  }
+
+  const successfulResponse = (response) => {
+    console.log(response);
+  }
+
+  const errorResponse = (response) => {
+    console.log(response);
+  }
 
   return(
     <div className="Welcome">
       <h1>Welcome { username }</h1>
       <div>
         Manage your todos - <Link to='/todos'>Go here</Link>
+      </div>
+      <div>
+        <button 
+          className="btn btn-success m-5" 
+          onClick={callHelloWorldApi}
+          >Call Hello World API</button>
       </div>
     </div>
   )
