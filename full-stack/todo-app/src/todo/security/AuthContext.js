7f10cbe7a52b23de971597ffa89ce0e68e06543e
Blairi <axel.avi.fer@gmail.com>
2023-05-18 22:56:25 -0600

    setting username into react context

--- full-stack/todo-app/src/todo/security/AuthContext.js
+++ full-stack/todo-app/src/todo/security/AuthContext.js
@@ -7,14 +7,17 @@ export const useAuth = () => useContext(AuthContext)
 export const AuthProvider = ({ children }) => {
 
   const [isAuthenticated, setAuthenticated] = useState(false);
+  const [username, setUsername] = useState(null);
 
   const login = (username, password) => {
-    if (username === 'blairi' && password === '') {
+    if (username === 'in28minutes' && password === '') {
       setAuthenticated(true);
+      setUsername(username);
       return true;
     }
     else{
       setAuthenticated(false);
+      setUsername(null);
       return false;
     }
   }
@@ -24,7 +27,7 @@ export const AuthProvider = ({ children }) => {
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, username }}>
       { children }
     </AuthContext.Provider>
   );
