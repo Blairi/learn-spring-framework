67358f4e0b96985d7dd38cffa12373cbc1075ab1
Blairi <axel.avi.fer@gmail.com>
2023-05-20 15:46:44 -0600

    implementing formik

--- full-stack/todo-app/src/todo/TodoComponent.jsx
+++ full-stack/todo-app/src/todo/TodoComponent.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
 import { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
 
 export const TodoComponent = () => {
 
@@ -10,6 +11,7 @@ export const TodoComponent = () => {
   const { username } = useAuth();
 
   const [description, setDescription] = useState('');
+  const [targetDate, setTargetDate] = useState('');
 
   useEffect(
     () => retrieveTodos()
@@ -20,15 +22,41 @@ export const TodoComponent = () => {
     retrieveTodoApi(username, id)
     .then(response => {
       setDescription(response.data.description);
+      setTargetDate(response.data.targetDate);
     })
     .catch(response => console.log(response));
+  }
+
+  const onSubmit = (values) => {
+    console.log(values);
   }
 
   return (
     <div className="container">
       <h1>Enter todo details</h1>
       <div>
-        Description: {description}
+        <Formik initialValues={{ description, targetDate }}
+          enableReinitialize={true}
+          onSubmit={ onSubmit }
+        >
+        {
+          (props) => (
+            <Form>
+              <fieldset className="form-group">
+                <label htmlFor="">Description</label>
+                <Field type="text" className="form-group" name="description" />
+              </fieldset>
+              <fieldset className="form-group">
+                <label htmlFor="">Target Date</label>
+                <Field type="date" className="form-group" name="targetDate" />
+              </fieldset>
+              <div>
+                <button className="btn btn-success" type="submit">Save</button>
+              </div>
+            </Form>
+          )
+        }
+        </Formik>
       </div>
     </div>
   )
