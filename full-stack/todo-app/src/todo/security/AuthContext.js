daf94d42881a591c1bb20b28405f56b2113348cd
Blairi <axel.avi.fer@gmail.com>
2023-05-23 11:21:07 -0600

    using jwt integration

--- full-stack/todo-app/src/todo/security/AuthContext.js
+++ full-stack/todo-app/src/todo/security/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
+import { executeBasicAuthenticationService, executeJwtAuthenticationService } from "../api/AuthenticationApiService";
 import { apiClient } from "../api/ApiClient";
 
 export const AuthContext = createContext();
@@ -25,23 +25,59 @@ export const AuthProvider = ({ children }) => {
   //   }
   // }
 
-  const login = async (username, password) => {
+  // const login = async (username, password) => {
 
-    const baToken = 'Basic ' + window.btoa(username + ':' + password);
+  //   const baToken = 'Basic ' + window.btoa(username + ':' + password);
+
+  //   try {
+
+  //     const response = await executeBasicAuthenticationService(baToken)
+  
+  //     if (response.status==200) {
+  //       setAuthenticated(true);
+  //       setUsername(username);
+  //       setToken(baToken);
+
+  //       apiClient.interceptors.request.use(
+  //         (config) => {
+  //           console.log("Intercepting and adding a token");
+  //           config.headers.Authorization = baToken;
+  //           return config;
+  //         }
+  //       );
+
+  //       return true;
+  //     }
+  //     else{
+  //       logout();
+  //       return false;
+  //     }
+
+  //   } catch (error) {
+  //       logout();
+  //       return false;
+  //   }
+    
+  // }
+
+  const login = async (username, password) => {
 
     try {
 
-      const response = await executeBasicAuthenticationService(baToken)
+      const response = await executeJwtAuthenticationService(username, password)
   
       if (response.status==200) {
+
+        const jwtToken = 'Bearer ' + response.data.token;
+
         setAuthenticated(true);
         setUsername(username);
-        setToken(baToken);
+        setToken(jwtToken);
 
         apiClient.interceptors.request.use(
           (config) => {
             console.log("Intercepting and adding a token");
-            config.headers.Authorization = baToken;
+            config.headers.Authorization = jwtToken;
             return config;
           }
         );
