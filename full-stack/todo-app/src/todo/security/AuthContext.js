3e6f4da4b7115b42210cc51626a4612370673498
Blairi <axel.avi.fer@gmail.com>
2023-05-22 18:08:10 -0600

    calling basic authentication service when logging

--- full-stack/todo-app/src/todo/security/AuthContext.js
+++ full-stack/todo-app/src/todo/security/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
 
 export const AuthContext = createContext();
 
@@ -9,17 +10,39 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setAuthenticated] = useState(false);
   const [username, setUsername] = useState(null);
 
+  // const login = (username, password) => {
+  //   if (username === 'in28minutes' && password === '') {
+  //     setAuthenticated(true);
+  //     setUsername(username);
+  //     return true;
+  //   }
+  //   else{
+  //     setAuthenticated(false);
+  //     setUsername(null);
+  //     return false;
+  //   }
+  // }
+
   const login = (username, password) => {
-    if (username === 'in28minutes' && password === '') {
-      setAuthenticated(true);
-      setUsername(username);
-      return true;
-    }
-    else{
-      setAuthenticated(false);
-      setUsername(null);
-      return false;
-    }
+
+    const baToken = 'Basic ' + window.btoa(username + ':' + password);
+
+    executeBasicAuthenticationService(baToken)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+    setAuthenticated(false);
+
+    // if (username === 'in28minutes' && password === '') {
+    //   setAuthenticated(true);
+    //   setUsername(username);
+    //   return true;
+    // }
+    // else{
+    //   setAuthenticated(false);
+    //   setUsername(null);
+    //   return false;
+    // }
   }
 
   const logout = () => {
